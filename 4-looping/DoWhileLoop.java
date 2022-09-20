a00e01471b6196171dec90cb8114cc95a15bd1b3
Stackofsugar <digno.christopher@gmail.com>
2022-09-20 09:41:30 +0700

    ADD Materials

---
+++ 4-looping/DoWhileLoop.java
@@ -0,0 +1,17 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("=== 1 ===");
+        do {
+            System.out.println("i = " + i);
+            i++;
+        } while (i < 5);
+
+        // System.out.println("=== 2 ===");
+        // i = 100;
+        // do {
+        //     System.out.println("i = " + i);
+        //     i++;
+        // } while (i < 5);
+    }
+}
---
+++ 4-looping/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // for (int i = 100; i >= 5; i /= 2) {
+        // System.out.println("i = " + i);
+        // }
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
