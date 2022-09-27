0b3c678f43c01f06dc81daa345d60a30d68988ba
Stackofsugar <digno.christopher@gmail.com>
2022-09-27 10:55:07 +0700

    Added insights about exception bubble-up

---
+++ 5-exception-handling/Example.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+        System.out.println(num);
+
+        scan.close();
+    }
+}
--- 5-exception-handling/ExceptionRaisingFunction.java
+++ 5-exception-handling/ExceptionRaisingFunction.java
@@ -6,9 +6,21 @@ public class ExceptionRaisingFunction {
         return (lhs / rhs);
     }
 
+    static int other(int lhs, int rhs) {
+        int division = divide(lhs, rhs);
+        return division - (lhs + rhs);
+    }
+
     public static void main(String[] args) {
-        System.out.println(divide(10, 5));
-        System.out.println(divide(0, 12));
-        System.out.println(divide(120, 0));
+        // System.out.println(divide(10, 5));
+        // System.out.println(divide(0, 12));
+        // try {
+        // System.out.println(divide(120, 0));
+        // } catch (Exception e) {
+        // System.out.println(e.getMessage());
+        // }
+        // System.out.println("Hello World");
+
+        System.out.println(other(10, 0));
     }
 }
